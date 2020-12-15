cc47de0965eb285a1c41a91a18390adeb6ae4f05
root <gsidsid@gmail.com>
2020-12-15 07:29:14 +0000

    adding changes for multi maze turtlebot

--- gym_gazebo/envs/turtlebot/gazebo_maze_multi_turtlebot_lidar.py
+++ gym_gazebo/envs/turtlebot/gazebo_maze_multi_turtlebot_lidar.py
@@ -40,6 +40,7 @@ class GazeboMazeMultiTurtlebotLidarEnv(gazebo_env.GazeboEnv):
         self.reward_range = (-np.inf, np.inf)
 
         self._seed()
+        self.last50actions = [0]*50
 
         self.img_rows = 32
         self.img_cols = 32
@@ -48,6 +49,16 @@ class GazeboMazeMultiTurtlebotLidarEnv(gazebo_env.GazeboEnv):
     def _seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
+
+    def in_view(self, gray_cv_image):
+        turtlebot_detected = False
+        # Detect a black
+        cv2.imshow("test", gray_cv_image)
+        ret, thresh = cv2.threshold(gray_cv_image, 25, 255, cv2.THRESH_BINARY_INV)
+        contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+        if len(contours) > 0:
+            turtlebot_detected = True
+        return turtlebot_detected
 
     def step(self, actionprep):
         action, prep = actionprep
@@ -81,6 +92,13 @@ class GazeboMazeMultiTurtlebotLidarEnv(gazebo_env.GazeboEnv):
             vel_cmd.linear.x = -0.4
             vel_cmd.angular.z = 0.0
             active_turtle.publish(vel_cmd)
+        
+        self.last50actions.pop(0)
+        if action in [0,3]:
+            self.last50actions.append(0)
+        else:
+            self.last50actions.append(1)
+        action_sum = sum(self.last50actions)
 
         # Use seeker vision to determine hider and seeker rewards
         seeker_image = None
@@ -106,22 +124,32 @@ class GazeboMazeMultiTurtlebotLidarEnv(gazebo_env.GazeboEnv):
         except (rospy.ServiceException) as e:
             print ("/gazebo/pause_physics service call failed")
 
+        cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+        cv_image = cv2.resize(cv_image, (self.img_rows, self.img_cols))
+
         hider_reward = 0
         seeker_reward = 0
 
         if not prep:
             # calculate hider seeker rewards from cv_image
-            pass
+            if self.in_view(cv_image):
+                print("IN VIEW")
+                seeker_reward += 1
+                hider_reward -= 1
+            else:
+                seeker_reward -= 1
+                hider_reward += 1
+            if action_sum > 40:
+                seeker_reward += 0.2
 
-        cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
-        cv_image = cv2.resize(cv_image, (self.img_rows, self.img_cols))
         state = cv_image.reshape(1, 1, cv_image.shape[0], cv_image.shape[1])
 
-        return state, (hider_reward, seeker_reward), (seeker_reward >= 10), {}
+        return state, hider_reward - seeker_reward, False, {}
 
 
     def reset(self):
-
+        
+        self.last50actions = [0]*50
         # Resets the state of the environment and returns an initial observation.
         rospy.wait_for_service('/gazebo/reset_simulation')
         try:
