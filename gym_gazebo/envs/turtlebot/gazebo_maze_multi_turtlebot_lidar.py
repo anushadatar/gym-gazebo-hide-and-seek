4c833ddb85e607f071c22763b24002aff2cf6ab5
root <gsidsid@gmail.com>
2020-12-17 06:04:38 +0000

    Corrected thresholding and for loop, added comments, updated refresh cycles

--- gym_gazebo/envs/turtlebot/gazebo_maze_multi_turtlebot_lidar.py
+++ gym_gazebo/envs/turtlebot/gazebo_maze_multi_turtlebot_lidar.py
@@ -1,25 +1,29 @@
 """
 Script associated with robot operation, computes and returns robot state for each step.
 """
-import gym
-import rospy
-import roslaunch
-import time
 import numpy as np
-import cv2
-import sys
 import os
 import random
+import sys
+import time
 
+# Gym dependencies.
+import gym
 from gym import utils, spaces
 from gym_gazebo.envs import gazebo_env
+from gym.utils import seeding
+
+# Ros Dependencies
 from geometry_msgs.msg import Twist
 from std_srvs.srv import Empty
 from sensor_msgs.msg import Image
 from sensor_msgs.msg import LaserScan
-from gym.utils import seeding
-from cv_bridge import CvBridge, CvBridgeError
+import rospy
+import roslaunch
 
+# CV Dependencies
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
 import skimage as skimage
 from skimage import transform, color, exposure
 from skimage.transform import rotate
@@ -28,32 +32,36 @@ from skimage.viewer import ImageViewer
 class GazeboMazeMultiTurtlebotLidarEnv(gazebo_env.GazeboEnv):
     """
     Environment associated with the multiple turtlebot hide and seek simulation.
+    Inherits from existing gazebo environment, so serves as the "environment" portion
+    required for a functioning OpenAI gym instance.
     """
     def __init__(self):
       	"""
-	Initialize the simulation, image processing variables, and rewards.
+	Initialize the simulation, image processing variables, and reward parameters.
 	"""
-	# Launch the simulation with the given launchfile name
+	# Launch the multiple turtlebot simulation.
         gazebo_env.GazeboEnv.__init__(self, "GazeboMazeMultiTurtlebotLidar_v0.launch")
-        self.t1_vel_pub = rospy.Publisher('/turtle_1/mobile_base/commands/velocity', Twist, queue_size=5)
-        self.t2_vel_pub = rospy.Publisher('/turtle_2/mobile_base/commands/velocity', Twist, queue_size=5)
+	# Publisher associated with the first turtlebot (the seeker).
+        self.seeker_publisher = rospy.Publisher('/turtle_1/mobile_base/commands/velocity', Twist, queue_size=5)
+	# Publisher associated with the second turtlebot (the hider). 
+        self.hider_publisher = rospy.Publisher('/turtle_2/mobile_base/commands/velocity', Twist, queue_size=5)
         self.unpause = rospy.ServiceProxy('/gazebo/unpause_physics', Empty)
         self.pause = rospy.ServiceProxy('/gazebo/pause_physics', Empty)
         self.reset_proxy = rospy.ServiceProxy('/gazebo/reset_simulation', Empty)
 
-        self.action_space = spaces.Discrete(4) #F,L,R,B
-        self.reward_range = (-np.inf, np.inf)
-
+	# Get the random seed for beginning environment operation.
         self._seed()
+	# Store the last 50 actions to intervene in the case of infinite looping.
         self.last50actions = [0]*50
-
+	self.action_space = spaces.Discrete(4)
+	
+	# Network image processing parameters.
         self.img_rows = 32
         self.img_cols = 32
         self.img_channels = 1
 	
-	self.hider_reward_increment = 1
-	self.seeker_reward_increment = 2
-
+	# Value by which to increment or decrement the seeker reward based on if the robot is visible/
+	self.reward_delta = 1
 
     def _seed(self, seed=None):
 	"""
@@ -65,26 +73,16 @@ class GazeboMazeMultiTurtlebotLidarEnv(gazebo_env.GazeboEnv):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
 
-    def in_view(self, gray_cv_image):
-	"""
-	Returns whether or not the other robot is in view.
-	gray_cv_image: The image associated with the robot's camera view.
-
-	Returns true if robot in view, false otherwise.
-	"""
-        turtlebot_detected = False
-        
-	#cv2.imshow("in_view test", gray_cv_image)
-        
-	#ret, thresh = cv2.threshold(gray_cv_image, 25, 255, cv2.THRESH_BINARY_INV)
-        #contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-        #if len(contours) > 0:
-        #    turtlebot_detected = True
-        return turtlebot_detected
-
     def step(self, actionprep):
 	"""
 	Update the reward values for each step.
+	actionprep = The state that the overall program is currently in (where
+		     "prep" indicates that the hider is hiding)
+	
+	Returns 
+	state      = 
+ 	reward     = 
+	done       = {}
 	"""
         action, prep = actionprep
         rospy.wait_for_service('/gazebo/unpause_physics')
@@ -93,9 +91,9 @@ class GazeboMazeMultiTurtlebotLidarEnv(gazebo_env.GazeboEnv):
         except (rospy.ServiceException) as e:
             print ("/gazebo/unpause_physics service call failed")
 
-        active_turtle = self.t2_vel_pub 
+        active_turtle = self.hider_publisher 
         if not prep:
-            active_turtle = self.t1_vel_pub
+            active_turtle = self.seeker_publisher
 
         if action == 0: # FORWARD
             vel_cmd = Twist()
@@ -125,17 +123,20 @@ class GazeboMazeMultiTurtlebotLidarEnv(gazebo_env.GazeboEnv):
             self.last50actions.append(1)
         action_sum = sum(self.last50actions)
 
-        # Use the seeker's vision to determine hider and seeker rewards
-        seeker_image = None
+        # Use the robot's vision to determine associated reward.
+	image = None
         success = False
         cv_image = None
-        while seeker_image is None or success is False:
+        while image is None or success is False:
             try:
-                seeker_image = rospy.wait_for_message('/turtle_1/camera/rgb/image_raw', Image, timeout=5)
-                h = seeker_image.height
-                w = seeker_image.width
-                cv_image = CvBridge().imgmsg_to_cv2(seeker_image, "bgr8")
-                # Temporal fix to check image is not corrupted.
+                if prep: # Get hider's camera if in prep mode.
+		     image = rospy.wait_for_message('/turtle_2/camera/rgb/image_raw', Image, timeout=5)
+                else: # Otherwise, simply use seeker's camera.
+		     image = rospy.wait_for_message('/turtle_1/camera/rgb/image_raw', Image, timeout=5)
+		h = image.height
+                w = image.width
+                cv_image = CvBridge().imgmsg_to_cv2(image, "bgr8")
+                # Check image is not corrupted.
                 if not (cv_image[h//2,w//2,0]==178 and cv_image[h//2,w//2,1]==178 and cv_image[h//2,w//2,2]==178):
                     success = True
                 else:
@@ -143,50 +144,50 @@ class GazeboMazeMultiTurtlebotLidarEnv(gazebo_env.GazeboEnv):
             except:
                 pass
 
+	# Pause physics.
         rospy.wait_for_service('/gazebo/pause_physics')
         try:
             self.pause()
         except (rospy.ServiceException) as e:
             print ("/gazebo/pause_physics service call failed")
+	
+	# Check to see if the other robot is in frame.
+	# Currently only used in seeking mode, but keeping both is useful for debugging.
 	in_view = False
-        if cv_image.min() < 60:
+	# Use a simple color threshold to check if the other robot is in frame.
+        if min(cv_image.flatten()) < 30:
 	    in_view = True
-	#hsv_image = cv2.cvtColor(cv_image. cv2.COLOR_BGR2HSV)
+	# Conver cv_image to colors and size needed for state update.
         cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
         cv_image = cv2.resize(cv_image, (self.img_rows, self.img_cols))
 	
-
-        hider_reward = 0
-        seeker_reward = 0
+	# Set reward and done values (used in the context of seeeking),
+        reward = 0
         done = False
-
         if not prep:
             # Calculate hider seeker rewards from cv_image
             if in_view:
-                #print("IN VIEW")
                 done = True
-                seeker_reward += self.seeker_reward_increment
-                hider_reward -= self.hider_reward_increment
+                reward += self.reward_delta
             else:
-                seeker_reward -= self.seeker_reward_increment
-                hider_reward += self.hider_reward_increment
+                reward -= self.reward_delta
+	    # Smooth out the reward delta as needed.
             if action_sum > 40:
-                seeker_reward += 0.2*self.seeker_reward_increment
-
+                reward += 0.2*self.reward_delta
+	# Reshape image and complete the state update.
         state = cv_image.reshape(1, 1, cv_image.shape[0], cv_image.shape[1])
-
-        return state, hider_reward - seeker_reward, done, {}
-
+        return state, reward, done, {}
 
     def reset(self):
         """
 	Reset the environment state.
+	Returns the state value as the associated CV image.
 	"""
+	# Zero out the last fifty actions tracker.
         self.last50actions = [0]*50
         # Resets the state of the environment and returns an initial observation.
         rospy.wait_for_service('/gazebo/reset_simulation')
         try:
-            #reset_proxy.call()
             self.reset_proxy()
         except (rospy.ServiceException) as e:
             print ("/gazebo/reset_simulation service call failed")
@@ -199,7 +200,7 @@ class GazeboMazeMultiTurtlebotLidarEnv(gazebo_env.GazeboEnv):
         except (rospy.ServiceException) as e:
             print ("/gazebo/unpause_physics service call failed")
 
-        # Use seeker vision to determine hider and seeker rewards
+        # Reset seeker vision as used to determine hider and seeker rewards.
         seeker_image = None
         success = False
         cv_image = None
@@ -209,7 +210,7 @@ class GazeboMazeMultiTurtlebotLidarEnv(gazebo_env.GazeboEnv):
                 h = seeker_image.height
                 w = seeker_image.width
                 cv_image = CvBridge().imgmsg_to_cv2(seeker_image, "bgr8")
-                #temporal fix, check image is not corrupted
+                # Temporary fix to check that image is not corrupted.
                 if not (cv_image[h//2,w//2,0]==178 and cv_image[h//2,w//2,1]==178 and cv_image[h//2,w//2,2]==178):
                     success = True
                 else:
@@ -217,14 +218,13 @@ class GazeboMazeMultiTurtlebotLidarEnv(gazebo_env.GazeboEnv):
             except:
                 pass
 
-        rospy.wait_for_service('/gazebo/pause_physics')
+	# Pause physics for final state update.
+	rospy.wait_for_service('/gazebo/pause_physics')
         try:
             self.pause()
         except (rospy.ServiceException) as e:
             print ("/gazebo/pause_physics service call failed")
-
         cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
         cv_image = cv2.resize(cv_image, (self.img_rows, self.img_cols))
         state = cv_image.reshape(1, 1, cv_image.shape[0], cv_image.shape[1])
-
         return state
